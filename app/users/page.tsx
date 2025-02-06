fd1b7ca22a40e11b0d07189b19e4880e7b980192
gamze <gamzekandara@gmail.com>
2025-02-05 19:46:57 -0500

    initial set up, routing learning

---
+++ app/users/page.tsx
@@ -0,0 +1,10 @@
+// React component that will be rendered when the user visits this page. /users
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
